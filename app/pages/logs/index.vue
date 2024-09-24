5e30a9d22ad2d46ba99fadd625d8a9d8050f9e1c
Gary Gong <yijhong@hotmail.com.tw>
2024-09-24 13:26:35 +0800

    log parsing adj.

--- app/pages/logs/index.vue
+++ app/pages/logs/index.vue
@@ -393,7 +393,7 @@ const clearFilter = () => {
             v-if="parseInt(row.proto) < protos.length"
             :color="protos[parseInt(row.proto)].color"
             :variant="protos[parseInt(row.proto)].variant"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             {{ row.proto }}
             {{ protos[parseInt(row.proto)].name }}
@@ -402,7 +402,7 @@ const clearFilter = () => {
             v-else
             color="green"
             variant="outline"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             {{ row.proto }}
           </UBadge>
@@ -415,7 +415,7 @@ const clearFilter = () => {
           <UBadge
             v-if="row.action === 'accept' || row.action === 'allow' || row.action === 'pass'"
             color="green"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -436,7 +436,7 @@ const clearFilter = () => {
           <UBadge
             v-else-if="row.action === 'block' || row.action === 'deny'"
             color="red"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -457,13 +457,13 @@ const clearFilter = () => {
           <UBadge
             v-else-if="row.action.startsWith('drop')"
             color="black"
-            class="gap-1 uppercase w-full justify-center"
+            class="gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="4"
+              stroke-width="2"
               stroke="currentColor"
               class="size-3"
             >
@@ -478,7 +478,7 @@ const clearFilter = () => {
           <UBadge
             v-else-if="row.action === 'reset' || row.action === 'server-rst' || row.action === 'client-rst'"
             color="orange"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -500,7 +500,7 @@ const clearFilter = () => {
             v-else-if="row.action === 'close'"
             color="black"
             variant="solid"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -521,7 +521,7 @@ const clearFilter = () => {
           <UBadge
             v-else
             color="white"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
             variant="outline"
           >{{ row.action }}</UBadge>
         </span>
@@ -534,7 +534,7 @@ const clearFilter = () => {
           <UBadge
             v-if="row.utmaction === 'accept' || row.utmaction === 'allow' || row.utmaction === 'pass'"
             color="green"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -553,9 +553,9 @@ const clearFilter = () => {
             {{ row.utmaction }}
           </UBadge>
           <UBadge
-            v-else-if="row.utmaction === 'block' || row.utmaction === 'deny' || row.utmaction === 'drop'"
+            v-else-if="row.utmaction === 'block' || row.utmaction === 'deny'"
             color="red"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -574,9 +574,30 @@ const clearFilter = () => {
             {{ row.utmaction }}
           </UBadge>
           <UBadge
+            v-else-if="row.utmaction.startsWith('drop')"
+            color="black"
+            class="gap-1 uppercase w-full justify-center justify-items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              class="size-3"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
+            <span class="font-bold">{{ row.utmaction }}</span>
+          </UBadge>
+          <UBadge
             v-else-if="row.utmaction === 'reset' || row.utmaction === 'server-rst' || row.utmaction === 'client-rst'"
             color="orange"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -598,7 +619,7 @@ const clearFilter = () => {
             v-else-if="row.utmaction === 'close'"
             color="black"
             variant="solid"
-            class="font-bold gap-1 uppercase w-full justify-center"
+            class="font-bold gap-1 uppercase w-full justify-center justify-items-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
